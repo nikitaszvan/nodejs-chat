08b96806dbd7c0f8d6b0176f48ae4422fe2048f1
Nikita Van <nikitaszvan@gmail.com>
2024-01-15 20:40:19 -0500

    create chat rooms

---
+++ slackClone/public/login.js
@@ -0,0 +1,8 @@
+let userName;
+let password;
+
+function login() {
+  const userName = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  window.location.href = '/slack.html';
+}
