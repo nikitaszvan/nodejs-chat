28d9f271303e405bef5f96cdcc7fac419e6335e3
Nikita Van <nikitaszvan@gmail.com>
2024-01-21 02:02:26 -0500

    redirection to main page

--- slackClone/index.js
+++ slackClone/index.js
@@ -105,14 +105,13 @@ app.post('/login', (req, res) => {
     if (filteredLogins.length === 0) {
         return res.status(401).json({ error: 'Incorrect credentials' });
     }
-    console.log(filteredLogins);
 
     const findPassword = filteredLogins.find(login => login.password === password);
     if (findPassword) {
         const accessToken = generateAccessToken(user);
         const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '10m' });
         refreshTokens.push(refreshToken);
-        res.json({ accessToken: accessToken, refreshToken: refreshToken });
+        return res.json({ success: true, redirectTo: '/index', accessToken: accessToken, refreshToken: refreshToken });
     }
     else {
         return res.status(401).json({ error: 'Incorrect credentials' });
@@ -129,6 +128,7 @@ function authenticateToken(req, res, next) {
     if (token == null) return res.sendStatus(401);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        console.log(token, process.env.ACCESS_TOKEN_SECRET);
         if (err) return res.sendStatus(403);
         req.user = user;
         next();
